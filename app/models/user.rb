ff47d9f6cee726ee96995038e8f3e6c3069b8c72
Abhishek Kumar <abhi11610192@gmail.com>
2019-08-09 13:54:20 +0530

    Complete Basic User Model (test and validation on password)

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
     validates :email, presence: true, length: { maximum: 50 }, format: { with: URI::MailTo::EMAIL_REGEXP },uniqueness: { case_sensitive: false }
     
     has_secure_password
+    validates :password, presence: true, length: { minimum: 6 }
 
 end
 
