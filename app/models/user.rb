94d23e7eacbf4a0d01c08f6673b489ccb31c976c
Abhishek Kumar <abhi11610192@gmail.com>
2019-08-09 13:44:41 +0530

    password added to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,8 @@ class User < ApplicationRecord
     before_save { email.downcase! }
     validates :name, presence: true, length: { maximum: 50 } 
     validates :email, presence: true, length: { maximum: 50 }, format: { with: URI::MailTo::EMAIL_REGEXP },uniqueness: { case_sensitive: false }
+    
+    has_secure_password
+
 end
 
---
+++ db/migrate/20190809081331_add_password_digest_to_users.rb
@@ -0,0 +1,7 @@
+# rails generate migration add_password_digest_to_users password_digest:string
+
+class AddPasswordDigestToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :password_digest, :string
+  end
+end
